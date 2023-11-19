40e68afb13832e0186ffd0f567e0dd5aa56d0edc
qcloud <ubuntu@localhost.localdomain>
2023-11-19 19:55:53 +0800

    初始化加载环境变量工具

--- src/dst_deploy/utils/env.py
+++ src/dst_deploy/utils/env.py
@@ -1,2 +1,11 @@
 """环境变量加载工具"""
 import os
+
+
+class Env:
+    """env加载工具"""
+
+    @classmethod
+    def string(cls, key: str, default: str) -> str:
+        """加载环境变量为string"""
+        return os.getenv(key, default=default)
